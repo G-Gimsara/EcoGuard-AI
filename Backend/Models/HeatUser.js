2cffe288294dd71d56dd429e31893b6441419c12
ChanukaIT22560544 <217544465+LAX-EM@users.noreply.github.com>
2026-02-09 10:17:20 +0530

    Refactor HeatUser model to use CommonJS syntax and update Postgres mapping

---
+++ Backend/Models/HeatUser.js
@@ -0,0 +1,78 @@
+/* import { DataTypes } from "sequelize";
+import sequelize from "../Config/sequelize_heat.js";
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+  },
+  location: {
+    type: DataTypes.STRING,
+  },
+}, {
+  tableName: "users",
+  timestamps: true,
+  underscored: true, // This maps "createdAt" in JS to "created_at" in Postgres
+});
+
+export default User; */
+
+const { DataTypes } = require("sequelize");
+const sequelize = require("../Config/sequelize_heat.js");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+  },
+  location: {
+    type: DataTypes.STRING,
+  },
+}, {
+  tableName: "users",
+  timestamps: true,
+  underscored: true, // "createdAt" -> "created_at" mapping  for  Postgres
+});
+
+
+module.exports = User;
