1914f1a2c079dbffb6136b355bb4ae70f90cd649
G.Gimsara <lenmicr250@gmail.com>
2026-03-07 16:40:04 -0800

    about page update

--- Frontend/app/Pages/Flood_Risk/about/page.tsx
+++ Frontend/app/Pages/Flood_Risk/about/page.tsx
@@ -5,7 +5,7 @@ import Navbar from "../NavBar/Navbar";
 
 export default function AboutNew() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50"> 
       <Navbar />
 
       <div className="max-w-7xl mx-auto px-6 py-8">
